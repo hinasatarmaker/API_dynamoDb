9d77048ecb3de6fff4a604cf57a6ef084d9cebbf
Hina Satarmaker <hina.satarmaker@cloudreach.com>
2023-01-05 10:49:24 +0530

    added new files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM public.ecr.aws/lambda/python:3.9
+COPY src/file1.py ${LAMBDA_TASK_ROOT}
+COPY src/app.py ${LAMBDA_TASK_ROOT}
+COPY src/config.py ${LAMBDA_TASK_ROOT}
+
+
+COPY requirements.txt  .
+RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
+
+CMD ["app.handler"]
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import file1 as dynamodb
 from flask import Flask
 from flask import request
+import serverless_wsgi
 
 app = Flask(__name__) 
 @app.route('/')
@@ -9,20 +10,6 @@ def root_route():
 #    app.run(host="localhost", port=5050, debug=True)
    dynamodb.create_table_movie()
    return 'Table created'
-
-@app.route('/movie', methods=['POST'])
-def add_movie():
-   data = request.get_json()
-   print(data)
-   response = dynamodb.write_to_movie(data['id'], data['title'], data['director'])   
-   if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-       return {
-           'msg': 'Add Movie successful',
-       }
-   return { 
-       'msg': 'error occurred',
-       'response': response
-   }
  
 @app.route('/getmovie/<id>', methods=['GET'])
 def get_movie(id):
@@ -37,32 +24,100 @@ def get_movie(id):
        'response': response
    }
 
+# @app.route('/movie', methods=['POST'])
+# def add_movie():
+#    data = request.get_json()
+#    print(data)
+#    response = dynamodb.write_to_movie(data['id'], data['title'], data['director'])   
+#    if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+#        return {
+#            'msg': 'Add Movie successful',
+#        }
+#    return { 
+#        'msg': 'error occurred',
+#        'response': response
+#    }
+@app.route('/movie', methods=['GET','POST'])
+def add_movie():
+#    print(data)
+#    response = dynamodb.write_to_movie(data['id'], data['title'], data['director'])   
+    data = request.get_json()
+    key_id = data['id']
+    response = dynamodb.read_from_movie(key_id)
+    if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+        if ('Item' in response):
+            return{
+                'msg': 'Item with same ID already present'
+            }
+        data = request.get_json()
+        response = dynamodb.write_to_movie(data['id'], data['title'], data['director'])  
+        return {
+           'msg': 'Add Movie successful'
+        }
+    return { 
+       'msg': 'error occurred',
+       'response': response
+   }
+
 @app.route('/updatemovie/<id>', methods=['GET','PUT'])
 def update_movie(id):
-    data = request.get_json()
-    response = dynamodb.update_in_movie(id, data)
+    # data = request.get_json()
+    # response = dynamodb.update_in_movie(id, data)
+    response = dynamodb.read_from_movie(id)
     if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-         return {
-           'msg'                : 'update successful',
-        #    'response'           : response['ResponseMetadata'],
-        #    'ModifiedAttributes' : response['Attributes']
-        }
+        if ('Item' in response):
+            data = request.get_json()
+            response = dynamodb.update_in_movie(id, data)
+            return {
+                'msg' : 'update successful'
+            }
+            # return { 'Item': response['Item'] }
+        return { 'msg' : 'Item not found!' }
     return {
-        'msg'      : 'error occurred',
-        #    'response' : response
-   } 
+           'msg'                : 'update successful',
+           'response'           : response['ResponseMetadata'],
+           'ModifiedAttributes' : response['Attributes']
+        }
+    # return {
+    #     'msg'      : 'error occurred',
+    #        'response' : response
+    # } 
+
+
+# @app.route('/updatemovie/<id>', methods=['GET','PUT'])
+# def update_movie(id):
+#     data = request.get_json()
+#     response = dynamodb.update_in_movie(id, data)
+#     if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+#         return {
+#            'msg'                : 'update successful',
+#            'response'           : response['ResponseMetadata'],
+#            'ModifiedAttributes' : response['Attributes']
+#         }
+#     return {
+#         'msg'      : 'error occurred',
+#            'response' : response
+#    } 
 
 @app.route('/deletemovie/<int:id>', methods=['DELETE'])
 def delete_movie(id):
-    response = dynamodb.delete_from_movie(id)
+    response = dynamodb.read_from_movie(id)
+    # response = dynamodb.delete_from_movie(id)
     if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-        return {
-            'msg': 'Deleted successfully',
-        }
+        if ('Item' in response):
+            response = dynamodb.delete_from_movie(id)
+            return {
+                'msg' : 'Deleted successfully'
+            }
+        return { 'msg' : 'Item not found!' }
     return {  
         'msg': 'Some error occcured',
         'response': response
     } 
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=5051, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5051, debug=True)
+
+def handler(event, context):
+	print("hello from lambda")
+	return serverless_wsgi.handle_request(app, event, context)
