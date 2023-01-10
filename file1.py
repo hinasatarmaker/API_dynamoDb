a784306ccfd7b8d111bec2064e8584e91e8ea8f3
Hina Satarmaker <hina.satarmaker@cloudreach.com>
2023-01-10 12:30:44 +0530

    chenges

--- file1.py
+++ file1.py
@@ -8,7 +8,7 @@ import os
 
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-REGION_NAME = "eu-west-1"
+REGION_NAME = 'eu-west-1'
 resource = resource(
    'dynamodb',
    aws_access_key_id     = AWS_ACCESS_KEY_ID,
